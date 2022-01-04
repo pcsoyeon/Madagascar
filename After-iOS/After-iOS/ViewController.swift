8e8e9543a875219f74a0b32a87e99ac5dfeeffd0
so yeon <alicesysy@swu.ac.kr>
2022-01-05 07:55:58 +0900

    [chore] 코드 정리

--- After-iOS/After-iOS/ViewController.swift
+++ After-iOS/After-iOS/ViewController.swift
@@ -6,14 +6,11 @@
 //
 
 import UIKit
-import Then
-import Kingfisher
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
